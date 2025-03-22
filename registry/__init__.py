46c19940ffe93b9d648d9e9b27b0a966e0f69b1c
Nidhir Bhavsar <75028682+Nid989@users.noreply.github.com>
2025-03-22 16:54:56 +0100

    Update: removed prompting regsitry (not a good idea)

--- registry/__init__.py
+++ registry/__init__.py
@@ -1,5 +1,4 @@
 from base import FunctionRegistry
-from edit_history import edit_history_function_registry
 from parsing import parse_function_registry
 
-__all__ = [FunctionRegistry, edit_history_function_registry, parse_function_registry]
+__all__ = [FunctionRegistry, parse_function_registry]
--- registry/edit_history.py
+++
@@ -1,7 +0,0 @@
-from typing import Callable, List, Dict, Any
-
-from registry.base import FunctionRegistry
-
-
-EditHistoryFuncType = Callable[[List[Dict], Any], List[Dict]]
-edit_history_function_registry = FunctionRegistry[EditHistoryFuncType]
